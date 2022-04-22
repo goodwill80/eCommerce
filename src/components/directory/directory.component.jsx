fb74257ad8475babfd43e144bbf55eec6a5c6836
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2022-04-22 17:23:37 +0800

    1st commit added categoryitem and directory components with style

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,22 @@
+import './directory.styles.scss'
+import CategoryItem from '../category-item/category-item.component'
+import categories from './categories.json'
+
+function Directory() {
+
+ const list = categories.map(({id, title, imageUrl})=>(
+    <CategoryItem
+        key={id}
+        title={title}
+        imageUrl={imageUrl}
+    />
+ ))
+
+  return (
+    <div className='directory-container'>
+        {list}
+    </div>
+  )
+}
+
+export default Directory
